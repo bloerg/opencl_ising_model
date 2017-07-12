232891377148b3338f8615eb6ad19cb0f4940303
Blörg <devel@bloerg.de>
2017-07-12 15:28:19 +0200

    first working program

--- Ising.cl
+++ Ising.cl
@@ -1,4 +1,9 @@
-__kernel void ising(global *char data) {
-    
+__kernel void ising(__global char* lattice) {
+    lattice[0] = 'H';
+    lattice[1] = 'e';
+    lattice[2] = 'l';
+    lattice[3] = 'l';
+    lattice[4] = 'o';
+    lattice[5] = '\n';
 
 }
--- Ising.cpp
+++ Ising.cpp
@@ -23,37 +23,69 @@ int main() {
     }
     
     //Use CPU (1) or GPU (0)
-    cl::Device default_device = all_devices[1];
-    std::cout << "Using device: " << default_device.getInfo<CL_DEVICE_NAME>() << "\n";
+    cl::Device device = all_devices[1];
+    std::cout << "Using device: " << device.getInfo<CL_DEVICE_NAME>() << "\n";
     
     
-    cl::Context context({default_device});
+    cl::Context context({device});
     
     // read the kernel from source file
     std::ifstream ising_kernel_file("Ising.cl");
-    std::string ising_kernel(
+    std::string ising_kernel_string(
         std::istreambuf_iterator<char>(ising_kernel_file),
         (std::istreambuf_iterator<char>())
     );
     
     //see http://github.khronos.org/OpenCL-CLHPP/
-    std::vector<std::string> program_strings {ising_kernel};
+    std::vector<std::string> program_strings {ising_kernel_string};
     
-    cl::Program ising_program(program_strings);
+    cl::Program ising_program(context, ising_kernel_string, true);
     
-    try {
-        ising_program.build("-cl-std=CL2.0");
+    //~ try {
+        //~ ising_program.build("-cl-std=CL2.0");
+        
+    //~ }
+    //~ catch (...) {
+        //~ // Print build info for all devices
+        //~ cl_int build_err = CL_SUCCESS;
+        //~ auto build_info = ising_program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(&build_err);
+        //~ for (auto &pair : build_info) {
+            //~ std::cerr << pair.second << std::endl << std::endl;
+        //~ }
+        //~ return 1;
+    //~ }
+    
+    if (ising_program.build({ device }) != CL_SUCCESS){
+        std::cout << " Error building: " << ising_program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(device) << "\n";
+        getchar();
+        exit(1);
     }
-    catch (...) {
-        // Print build info for all devices
-        cl_int build_err = CL_SUCCESS;
-        auto build_info = ising_program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(&build_err);
-        for (auto &pair : build_info) {
-            std::cerr << pair.second << std::endl << std::endl;
-        }
-        return 1;
-    }
     
+        //~ char lattice[16];
+        //~ cl::Buffer lattice_buffer(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(lattice));
+        //~ cl::Kernel ising_kernel(ising_program, "Ising", &err);
+        //~ ising_kernel.setArg(0, lattice_buffer);
+        
+        //~ cl::CommandQueue queue(context, device);
+        
+        //~ queue.enqueueReadBuffer(lattice_buffer, CL_TRUE, 0, sizeof(lattice), lattice);
+        //~ std::cout << lattice;
+        //~ std::cin.get(); // wait for keypress
+    
+        char lattice[16];
+    
+        cl::Buffer lattice_buffer(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(lattice));
+        cl::CommandQueue queue(context, device);
+        
+        
+        cl::Kernel ising_kernel(ising_program, "ising");
+        ising_kernel.setArg(0, lattice_buffer);
+        
+        //~ queue.enqueueNDRangeKernel(ising_kernel, cl::NullRange, cl::NullRange, cl::NullRange);
+        queue.enqueueNDRangeKernel(ising_kernel, cl::NDRange(0), cl::NDRange(10), cl::NDRange(1));
 
-    
+        queue.enqueueReadBuffer(lattice_buffer, CL_TRUE, 0, sizeof(lattice), lattice);
+        std::cout << lattice;
+        std::cin.get(); // wait for keypress
+
 }
